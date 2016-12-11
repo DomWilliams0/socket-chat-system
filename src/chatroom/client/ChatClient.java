9141671b61cfff5cd8d6f77c6f513cd0136c4605
Dom Williams <me@domwillia.ms>
2016-12-11 18:07:41 +0100

    Extract networking from ChatClient

--- src/chatroom/client/ChatClient.java
+++ src/chatroom/client/ChatClient.java
@@ -1,21 +1,13 @@
 package chatroom.client;
 
-import chatroom.server.Message;
-import chatroom.shared.ChatException;
-import chatroom.shared.Logger;
-import chatroom.shared.protocol.*;
+import chatroom.shared.protocol.Protocol;
 
-import java.io.*;
-import java.net.Socket;
-import java.util.List;
 import java.util.Scanner;
 
 public class ChatClient
 {
 	private final String username;
-
-	private BufferedReader in;
-	private BufferedWriter out;
+	private final ClientConnection connection;
 
 	/**
 	 * @param username The client's username to use in the chatroom
@@ -29,125 +21,25 @@ public class ChatClient
 			username.contains(Protocol.DELIMITER) ||
 			username.equals(Protocol.SERVER_USERNAME))
 			throw new IllegalArgumentException("Invalid username");
+
+		this.connection = new ClientConnection(this);
 	}
 
-	/**
-	 * Connect to a chat server
-	 *
-	 * @param address The server's address
-	 * @param port    The server's port
-	 * @return If the connection was a success
-	 */
-	public boolean connect(String address, int port)
+	public static void main(String[] args)
 	{
-		boolean success = false;
-		try
-		{
-			String addrStr = address + ":" + port;
+		if (args.length != 3)
+			throw new IllegalArgumentException("Usage: <username> <address> <port>");
 
-			Logger.log(String.format("Attempting to connect to %s...", addrStr));
-			Socket socket = new Socket(address, port);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		String username = args[0];
+		String address = args[1];
+		Integer port = Integer.parseInt(args[2]);
 
-			Logger.log("Successfully connected");
+		ChatClient client = new ChatClient(username);
+		boolean success = client.start(address, port);
 
-			// send join command
-			sendJoin();
-
-			// request user list
-			sendList();
-
-			// request history
-			sendHistory();
-
-			success = true;
-		} catch (IOException e)
-		{
-			Logger.error("Could not connect to server: %s", e.getMessage());
-		} catch (ChatException e)
-		{
-			e.printStackTrace();
-		}
-
-		return success;
+		System.exit(success ? 0 : 1);
 	}
 
-	public void disconnect()
-	{
-		if (out == null && in == null)
-			return;
-
-		try
-		{
-			Logger.log("Disconnecting");
-			CommandQuit command = new CommandQuit(username);
-			command.send(out);
-
-			out = null;
-			in = null;
-
-		} catch (ChatException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public void sendMessage(String message)
-	{
-		Message m = new Message(username, message);
-		m.encode();
-
-		CommandSend command = new CommandSend(m);
-		try
-		{
-			command.send(out);
-		} catch (ChatException e)
-		{
-			e.printStackTrace();
-		}
-
-		// no ack needed for now
-	}
-
-	private void sendJoin() throws ChatException
-	{
-		CommandJoin command = new CommandJoin(username);
-		command.send(out);
-
-		String ack = CommandAck.readAck(in);
-		if (ack != null)
-		{
-			display("Error while connecting: %s", ack);
-			return;
-		}
-
-		// read banner
-		String banner = command.read(in);
-		display("The server says: %s", banner);
-	}
-
-	private void sendList() throws ChatException
-	{
-		CommandList command = new CommandList(username);
-		command.send(out);
-
-		String list = command.read(in);
-		display(list);
-	}
-
-	private void sendHistory() throws ChatException
-	{
-		CommandClientHistory command = new CommandClientHistory(username);
-		command.send(out);
-
-		List<Message> messages = command.readAndParse(in);
-		messages.forEach(this::onReceiveMessage);
-	}
-
-	/**
-	 * @return The client's username
-	 */
 	public String getUsername()
 	{
 		return username;
@@ -156,7 +48,7 @@ public class ChatClient
 	/**
 	 * Displays the given message on the UI
 	 */
-	private void display(String message, Object... format)
+	public void display(String message, Object... format)
 	{
 		System.out.printf(message + "\n", format);
 	}
@@ -164,18 +56,12 @@ public class ChatClient
 	public boolean start(String address, int port)
 	{
 		// connect to server
-		if (!connect(address, port))
+		if (!connection.connect(address, port))
 		{
 			return false;
 		}
 
-		// register signal handler
-		Runtime.getRuntime().addShutdownHook(new Thread(this::disconnect));
-
-		// start receiving thread
-		startReceivingThread();
-
-		// send messages
+		// TODO UI interface
 		display("Type /quit to exit");
 		Scanner scanner = new Scanner(System.in);
 		String line;
@@ -194,49 +80,13 @@ public class ChatClient
 			}
 
 			// send message
-			sendMessage(line);
+			connection.sendMessage(line);
 		}
 
 		// disconnect
-		disconnect();
+		connection.disconnect();
 
 		return true;
 	}
 
-	private void startReceivingThread()
-	{
-		Thread thread = new Thread(new ClientMessageReceiver(this));
-		thread.setDaemon(true);
-		thread.start();
-	}
-
-	public BufferedReader getIn()
-	{
-		return in;
-	}
-
-	public BufferedWriter getOut()
-	{
-		return out;
-	}
-
-	public void onReceiveMessage(Message m)
-	{
-		display("[%s]: %s", m.getFrom(), m.getContent());
-	}
-
-	public static void main(String[] args)
-	{
-		if (args.length != 3)
-			throw new IllegalArgumentException("Usage: <username> <address> <port>");
-
-		String username = args[0];
-		String address = args[1];
-		Integer port = Integer.parseInt(args[2]);
-
-		ChatClient client = new ChatClient(username);
-		boolean success = client.start(address, port);
-
-		System.exit(success ? 0 : 1);
-	}
 }
---
+++ src/chatroom/client/ClientConnection.java
@@ -0,0 +1,200 @@
+package chatroom.client;
+
+import chatroom.server.Message;
+import chatroom.shared.ChatException;
+import chatroom.shared.Logger;
+import chatroom.shared.protocol.*;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ClientConnection
+{
+	private BufferedReader in;
+	private BufferedWriter out;
+
+	private final ChatClient client;
+
+	public ClientConnection(ChatClient client)
+	{
+		this.client = client;
+	}
+
+	/**
+	 * Connect to a chat server
+	 *
+	 * @param address The server's address
+	 * @param port    The server's port
+	 * @return If the connection was a success
+	 */
+	public boolean connect(String address, int port)
+	{
+		boolean success = false;
+		try
+		{
+			String addrStr = address + ":" + port;
+
+			Logger.log(String.format("Attempting to connect to %s...", addrStr));
+			Socket socket = new Socket(address, port);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+			Logger.log("Successfully connected");
+
+			// send join command
+			sendJoin();
+
+			// register signal handler
+			Runtime.getRuntime().addShutdownHook(new Thread(this::disconnect));
+
+			// request user list
+			sendList();
+
+			// request history
+			sendHistory();
+
+			// start receiving thread
+			startReceivingThread();
+
+			success = true;
+
+		} catch (IOException e)
+		{
+			Logger.error("Could not connect to server: %s", e.getMessage());
+		} catch (ChatException e)
+		{
+			e.printStackTrace();
+		}
+
+		return success;
+	}
+
+	public void disconnect()
+	{
+		if (out == null && in == null)
+			return;
+
+		try
+		{
+			Logger.log("Disconnecting");
+			CommandQuit command = new CommandQuit(client.getUsername());
+			command.send(out);
+
+			out = null;
+			in = null;
+
+		} catch (ChatException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+	public void sendMessage(String message)
+	{
+		Message m = new Message(client.getUsername(), message);
+		m.encode();
+
+		CommandSend command = new CommandSend(m);
+		try
+		{
+			command.send(out);
+		} catch (ChatException e)
+		{
+			e.printStackTrace();
+		}
+
+		// no ack needed for now
+	}
+
+	private void sendJoin() throws ChatException
+	{
+		CommandJoin command = new CommandJoin(client.getUsername());
+		command.send(out);
+
+		String ack = CommandAck.readAck(in);
+		if (ack != null)
+		{
+			client.display("Error while connecting: %s", ack);
+			return;
+		}
+
+		// read banner
+		String banner = command.read(in);
+		client.display("The server says: %s", banner);
+	}
+
+	private void sendList() throws ChatException
+	{
+		CommandList command = new CommandList(client.getUsername());
+		command.send(out);
+
+		String list = command.read(in);
+		client.display(list);
+	}
+
+	private void sendHistory() throws ChatException
+	{
+		CommandClientHistory command = new CommandClientHistory(client.getUsername());
+		command.send(out);
+
+		List<Message> messages = command.readAndParse(in);
+		messages.forEach(this::onReceiveMessage);
+	}
+
+	private void startReceivingThread()
+	{
+		Thread thread = new Thread(new ClientMessageReceiver(in, this::onReceiveMessage));
+		thread.setDaemon(true);
+		thread.start();
+	}
+
+	private void onReceiveMessage(Message m)
+	{
+		client.display("[%s]: %s", m.getFrom(), m.getContent());
+	}
+
+
+	private class ClientMessageReceiver implements Runnable
+	{
+		private final BufferedReader instream;
+		private final Consumer<Message> onReceiveMessage;
+
+		ClientMessageReceiver(BufferedReader instream, Consumer<Message> onReceiveMessage)
+		{
+			this.instream = instream;
+			this.onReceiveMessage = onReceiveMessage;
+		}
+
+		@Override
+		public void run()
+		{
+			// wait for messages
+			while (true)
+			{
+				// TODO needs to be interrupted on disconnect
+
+				try
+				{
+					RequestPrologue request = Command.readPrologue(instream, Opcode.SEND);
+
+					String encodedMessage = CommandSend.readMessageContent(instream);
+					Message m = new Message(request.getUsername(), encodedMessage);
+					m.decode();
+
+					onReceiveMessage.accept(m);
+				} catch (ChatException e)
+				{
+					e.printStackTrace();
+
+					if (e.isSerious())
+						break;
+				}
+			}
+
+		}
+	}
+
+}
