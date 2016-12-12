ad48ef14391bfdc9d660407fa4804eff46528aa5
Dom Williams <me@domwillia.ms>
2016-12-12 13:02:47 +0100

    Add null check to logging

--- src/chatroom/shared/Logger.java
+++ src/chatroom/shared/Logger.java
@@ -9,6 +9,9 @@ public class Logger
 
 	public static void error(String message, Object... format)
 	{
+		if (message == null)
+			return;
+
 		System.out.flush();
 		System.err.printf("[ERR] " + message + "\n", format);
 		System.err.flush();
@@ -16,6 +19,9 @@ public class Logger
 
 	public static void log(String message, Object... format)
 	{
+		if (message == null)
+			return;
+
 		System.err.flush();
 		System.out.printf("[LOG] " + message + "\n", format);
 		System.out.flush();
